68e43fab6991bcc02b04e9e5d1f55a77fcad9f88
Yana <yanassam81@gmail.com>
2024-04-08 21:50:50 +0200

    feat: solutions

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,50 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', addResult);
+
+function addResult(event) {
+  event.preventDefault();
+
+  const checkedInput = form.querySelector('input[name="state"]:checked').value;
+  const input = form.querySelector('input[name="delay"]');
+  const delay = Number(input.value);
+
+  function checkValue(checkedInput) {
+    return new Promise((resolve, reject) => {
+      if (checkedInput === 'rejected') {
+        return setTimeout(
+          () => reject(`Rejected promise in ${delay}ms`),
+          delay
+        );
+      }
+      setTimeout(
+        () => resolve(`\u2705 Fulfilled promise in ${delay}ms`),
+        delay
+      );
+    });
+  }
+
+  checkValue(checkedInput)
+    .then(response => {
+      console.log(response);
+      iziToast.success({
+        message: response,
+        position: 'topRight',
+        iconUrl: null,
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      iziToast.error({
+        message: error,
+        position: 'topRight',
+      });
+    });
+
+  form.reset();
+}
