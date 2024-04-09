b23f14ff7d2b56e34fb25c6c1e3007459140a3ca
Yana <yanassam81@gmail.com>
2024-04-09 21:34:30 +0200

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,9 +3,6 @@ import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
-import errorIcon from '../img/bi_x-octagon.svg';
-import okIcon from '../img/bi_check2-circle.svg';
-
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', addResult);
@@ -17,33 +14,29 @@ function addResult(event) {
   const input = form.querySelector('input[name="delay"]');
   const delay = Number(input.value);
 
-  function checkValue(checkedInput) {
-    return new Promise((resolve, reject) => {
-      if (checkedInput === 'rejected') {
-        return setTimeout(
-          () => reject(`Error Rejected promise in ${delay}ms`),
-          delay
-        );
-      }
-      setTimeout(() => resolve(`OK Fulfilled promise in ${delay}ms`), delay);
-    });
-  }
+  const promise = new Promise((resolve, reject) => {
+    if (checkedInput === 'rejected') {
+      return setTimeout(
+        () => reject(`❌ Rejected promise in ${delay}ms`),
+        delay
+      );
+    }
+    setTimeout(() => resolve(`✅ Fulfilled promise in ${delay}ms`), delay);
+  });
 
-  checkValue(checkedInput)
+  promise
     .then(response => {
-      // console.log(response);
       iziToast.success({
         message: response,
         position: 'topRight',
-        iconUrl: okIcon,
+        // iconUrl: okIcon,
       });
     })
     .catch(error => {
-      // console.log(error);
       iziToast.error({
         message: error,
         position: 'topRight',
-        iconUrl: errorIcon,
+        // iconUrl: errorIcon,
       });
     });
 
